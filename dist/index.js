150eac8371f54c860b904d4a23975f2475933cc0
Theodor Borgman <tb222yz@student.lnu.se>
2022-09-17 15:41:21 +0200

    add test command. Export Functions.

--- dist/index.js
+++ dist/index.js
@@ -2,28 +2,48 @@ import http from 'http'
 import https from 'https'
 
 
-const url = 'https://www.klart.se/se/v%C3%A4stra-g%C3%B6talands-l%C3%A4n/v%C3%A4der-g%C3%B6teborg/'
-
+/**
+ * Returns promise, resulting in a string, will take HTTP or HTTPS url's.
+ *
+ * @param {string} url - URL to get body from.
+ * @returns Promise - when resolved this is a string.
+ */
 const getHtmlBody = async (url) => {
-
   return new Promise(resolve => {
-      https.get(url, response => {
-        let responseData = ''
-      
-        response.on('data', (dataChunk) => {
-          responseData += dataChunk;
+      console.log(isHttps(url))
+      if (isHttps(url)){
+        https.get(url, response => {
+          let responseData = ''
+          response.on('data', (dataChunk) => responseData += dataChunk)
+          response.on('end', () => resolve(responseData))
         })
-
-        response.on('end', () => {
-          resolve(responseData)
-        })
-      })
+      }
   })
 }
 
-async function getData() {
-  const htmlBody = await getHtmlBody(url)
-  console.log(htmlBody)
+
+// EXPORTED FUNCTIONS
+
+const isHttps = (url) => {
+  const protocol = url.split(':')[0]
+  if (protocol.toLowerCase() === 'https'){
+    return true
+  }
+  return false
 }
 
-export default getData
+/**
+ * 
+ * @param {string} url 
+ */
+const getHtmlData = async(url) => {
+  const htmlBody = await getHtmlBody(url)
+  return htmlBody
+}
+
+const parseData = () => {
+  console.log('usuck')
+}
+
+
+export {getHtmlData, parseData, isHttps}
---
+++ function-test-app/test.js
@@ -0,0 +1,14 @@
+import {parseData, getHtmlData} from '../dist/index.js'
+
+const url = 'https://www.klart.se/se/v%C3%A4stra-g%C3%B6talands-l%C3%A4n/v%C3%A4der-g%C3%B6teborg/'
+
+const data = getHtmlData(url)
+parseData()
+
+// Test is http
+
+// Test is https
+
+// Test getting data
+
+// 
